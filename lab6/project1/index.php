a50d2274d6991bd3d7772b692828d44d017b5d4f
Olasunkanmi Oluwadara <77937202+soonkanmi@users.noreply.github.com>
2021-02-09 18:41:41 +0000

    Adding Lab 6

---
+++ lab6/project1/index.php
@@ -0,0 +1,21 @@
+<?php
+    echo "Hello again, this is using vscode "
+?>
+
+<?php
+echo "I get printed!";
+// I don't! I'm a comment.//
+    ?>
+
+    <?php
+    echo"hello, world!";
+    ?>
+    
+
+    <?php
+    $myname= "Frodo Baggins ";
+    $myage= 111;
+    echo "my name is" .$myname. "and I am" .$myage;
+    ?>
+
+
